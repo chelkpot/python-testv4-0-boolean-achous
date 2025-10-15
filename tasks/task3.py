033777403c55812f902ed9e4c4aa5b791e2eff86
student <student@corp.kpiot.ru>
2025-10-15 15:10:38 +0500

    fsafsaf

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
+     a=str(input())
+     b=str(input())
+     print(a=="awesome" or b=="awesome")
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
